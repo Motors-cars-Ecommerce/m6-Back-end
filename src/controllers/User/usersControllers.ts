663e535366bbfb0691bc8ef5d62b40fe80653e7c
Talles Simao <102775805+Tallesco201@users.noreply.github.com>
2023-04-10 11:34:50 -0300

    Feat:I created the HTTP GET method for user list.

--- src/controllers/User/usersControllers.ts
+++ src/controllers/User/usersControllers.ts
@@ -1,9 +1,16 @@
 import { Request, Response} from "express"
 import { IUserRequest } from "../../interfaces/Users/user.Interface"
 import { createdUserService } from "../../services/User/createdUserService"
+import { getUserService } from "../../services/User/getUserService"
 
 export const createdUserController = async (req:Request, res:Response)=>{
     const userDataBody:IUserRequest = req.body
     const newUser =  await createdUserService(userDataBody)
     return res.status(201).json(newUser)
+}
+
+export const getUserController = async (req:Request, res:Response) => {
+    const listUsers = await getUserService()
+
+    return res.status(200).json(listUsers)
 }
--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -1,9 +1,14 @@
 import {Router} from "express"
-import { createdUserController } from "../controllers/User/usersControllers"
+import { createdUserController, getUserController } from "../controllers/User/usersControllers"
 
 const userRouter = Router()
 
 //Criação de usuario 
 userRouter.post("",createdUserController)
+
+//Leitura de todos usuarios
+userRouter.get("", getUserController)
+
+
 
 export default userRouter
