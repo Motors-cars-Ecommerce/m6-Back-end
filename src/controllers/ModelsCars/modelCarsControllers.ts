4248fd1796f79915167cb4c0ec709f9e57774d67
Talles Simao <102775805+Tallesco201@users.noreply.github.com>
2023-04-13 21:55:18 -0300

    Feat: I cried two GET where one searches for the model and returns all the cars of the same model and another search for the id

--- src/controllers/ModelsCars/modelCarsControllers.ts
+++ src/controllers/ModelsCars/modelCarsControllers.ts
@@ -1,11 +1,32 @@
 import { Request, Response} from "express"
-import { ICarModelRequest } from "../../interfaces/ModelsCars/models.interface";
+import { ICarModelRequest, ICarModelRequestGetModel } from "../../interfaces/ModelsCars/models.interface";
 import { createdModelsCarsService } from "../../services/modelsCars/createdModelCarsService";
+import { listCarsModelsService } from "../../services/modelsCars/listCarsModelsService";
+import { listCarByIdService } from "../../services/modelsCars/listCarByIdService";
+import { deleteCarByIdService } from "../../services/modelsCars/deleteCarByIdService";
 
 
 export const createdModelController = async (req:Request, res:Response)=>{
     const dataBody: ICarModelRequest = req.body;
     const createdModel = await createdModelsCarsService(dataBody)
     return res.status(201).json(createdModel)
+}
+
+export const listCarsModelsController = async (req:Request, res:Response)=>{
+    const dataBody: ICarModelRequestGetModel= req.body;
+    const service = await listCarsModelsService(dataBody)
+    return res.status(200).json(service)
+}
+
+export const listCarByIdController = async (req:Request, res:Response)=>{
+    const id:string = req.params.id
+    const car = await listCarByIdService(id)
+    return res.status(200).json(car)
+}
+
+export const deleteCarByIdController = async (req:Request, res:Response)=>{
+    const id:string = req.params.id
+    const deleteCar= await deleteCarByIdService(id)
+    return res.status(204).json(deleteCar)
 }
 
--- src/routes/modelsCarsRoutes.ts
+++ src/routes/modelsCarsRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createdModelController } from "../controllers/ModelsCars/modelCarsControllers";
+import { createdModelController, deleteCarByIdController, listCarByIdController, listCarsModelsController } from "../controllers/ModelsCars/modelCarsControllers";
 import { ensureAuthMiddleware } from "../middleware/ensureAuth.middleware";
 
 
@@ -7,6 +7,15 @@ const modelRouter = Router()
 
 //Criar model 
 modelRouter.post("", ensureAuthMiddleware,createdModelController)
+
+//lista de todos os carros referentes a modelo
+modelRouter.get("", ensureAuthMiddleware,listCarsModelsController)
+
+//lista de carro pelo id
+modelRouter.get("/:id", ensureAuthMiddleware, listCarByIdController)
+
+//Deletar carro pelo id 
+modelRouter.delete("/:id", ensureAuthMiddleware, deleteCarByIdController)
 
 
 export default modelRouter
