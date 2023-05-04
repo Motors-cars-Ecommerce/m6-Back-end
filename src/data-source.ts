04e4b5c08bf597be1b94c8c9ec3b4a63511e5a49
Matheus Carneiro <mtscrn21@gmail.com>
2023-05-04 19:37:52 -0300

    fix render

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,35 +1,42 @@
-import "reflect-metadata";
 import "dotenv/config";
+import "reflect-metadata";
 import { DataSource, DataSourceOptions } from "typeorm";
-import Address from "./entities/address.entity";
-import Car from "./entities/car.entity";
-import Comment from "./entities/comment.entity";
-import Image from "./entities/image.entity";
-import ModelsCar from "./entities/modelCar.entity";
-import User from "./entities/user.entity";
-import { CreateTables01683207913929 } from "./migrations/1683207913929-createTables0";
-import { OnDelete1683222473681 } from "./migrations/1683222473681-onDelete";
+import path from "path";
 
-const AppDataSource = new DataSource(
-  process.env.NODE_ENV === "test"
-    ? {
-        type: "sqlite",
-        database: ":memory:",
-        synchronize: true,
-        entities: [],
-      }
-    : {
-        type: "postgres",
-        host: process.env.PGHOST!,
-        port: parseInt(process.env.PGPORT!),
-        username: process.env.PGUSER!,
-        password: process.env.PGPASSWORD!,
-        database: process.env.PGDATABASE!,
-        logging: true,
-        synchronize: false,
-        entities: [Address, Car, Comment, Image, ModelsCar, User],
-        migrations: [CreateTables01683207913929, OnDelete1683222473681],
-      }
-);
+const dataSourceConfig = (): DataSourceOptions => {
+  const entitiesPath: string = path.join(__dirname, "./entities/**.{ts,js}");
+  const migrationsPath: string = path.join(
+    __dirname,
+    "./migrations/**.{ts,js}"
+  );
+
+  const dbUrl: string | undefined = process.env.DATABASE_URL;
+
+  if (!dbUrl) {
+    throw new Error("Env var DATABASE_URL does not exists");
+  }
+
+  const nodeEnv: string | undefined = process.env.NODE_ENV;
+
+  if (nodeEnv === "test") {
+    return {
+      type: "sqlite",
+      database: ":memory:",
+      synchronize: true,
+      entities: [entitiesPath],
+    };
+  }
+
+  return {
+    type: "postgres",
+    url: dbUrl,
+    synchronize: false,
+    logging: true,
+    migrations: [migrationsPath],
+    entities: [entitiesPath],
+  };
+};
+
+const AppDataSource = new DataSource(dataSourceConfig());
 
 export default AppDataSource;
